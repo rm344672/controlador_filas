d7c32b6e41f2abc94a361d0996e9525056017c1d
Bruno Ribeiro <brunoesperancim@hotmail.com>
2022-10-12 06:17:02 -0300

    Adicionando contagem da fila no BD

--- lib/models/filas.dart
+++ lib/models/filas.dart
@@ -1,12 +1,29 @@
-import 'dart:ffi';
+
 import 'package:cloud_firestore/cloud_firestore.dart';
 
 import 'package:gestor_fila/models/usuarios.dart';
 
-class filas {
-  int? id;
-  final int lugar;
-  final Usuario user;
+class Filas {
+  int? id_fila;
+  final int? id_atual;
+  final int? id_ultimo;
 
-  filas({this.id, required this.lugar, required this.user});
+  DocumentReference? reference;
+
+
+  Filas({this.id_fila, required this.id_atual, required this.id_ultimo});
+
+      Filas.fromMap(Map<String, dynamic> map, {this.reference})
+      : id_fila = map['id_fila'],
+        id_atual = map['id_atual'],
+        id_ultimo = map['id_ultimo'];
+
+
+    Map<String, dynamic> toJson() =>
+      {"id_fila": id_fila, "id_atual": id_atual, "fim": id_ultimo};
+
+    Filas.fromSnapshot(QueryDocumentSnapshot snapshot)
+      : this.fromMap(snapshot.data() as Map<String, dynamic>,
+            reference: snapshot.reference);
 }
+
